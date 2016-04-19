30256119526c792ae7585ad4406ea8dd06835718
slimrapper <slimrapper112@gmail.com>
2016-04-19 01:03:12 -0500

    actividad_8

---
+++ project/src/RadioOg/Conexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RadioOg;
+import java.sql.*;
+import javax.swing.JOptionPane;
+        
+/**
+ *
+ * @author Slim
+ */
+public class Conexion {
+   Connection con=null;
+   public Connection conexion(){
+       try {
+           //cargar nuestro driver
+           Class.forName("com.mysql.jdbc.Driver");
+           con=DriverManager.getConnection("jdbc:mysql://localhost/radioog","root","");
+           System.out.println("conexion establecida");
+           JOptionPane.showMessageDialog(null, "conexion establecida");
+       } catch (ClassNotFoundException | SQLException e) {
+           System.out.println("error de conexion");
+           JOptionPane.showMessageDialog(null, "error de conexion "+e);
+       }
+       return con;
+   }
+}
